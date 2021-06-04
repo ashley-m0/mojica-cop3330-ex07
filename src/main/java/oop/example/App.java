e015920029dd15a11e4643d2b0df94d48f63a1f0
ashley-m0 <ashley.mojica@knights.ucf.edu>
2021-06-04 10:33:46 -0400

    Update 1 6/4

--- src/main/java/oop/example/App.java
+++ src/main/java/oop/example/App.java
@@ -1,6 +1,12 @@
 package oop.example;
 
 import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Ashley Mojica
+ */
+
 /*
 Exercise 7 - Area of a Rectangular Room
 When working in a global environment, you’ll have to present information in both metric and Imperial
@@ -13,7 +19,7 @@ Example Output:
 What is the length of the room in feet? 15
 What is the width of the room in feet? 20
 You entered dimensions of 15 feet by 20 feet.
-The area is 
+The area is
 300 square feet
 27.871 square meters
 
@@ -34,6 +40,44 @@ public class App
     static Scanner input = new Scanner(System.in);
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App myApp = new App();
+        double feetConversion = 1.0; // Because it's feet->feet
+        double metersConversion =  0.09290304;
+        String lengthString = myApp.getLength();
+        String widthString = myApp.getWidth();
+
+        double length = myApp.convertStringToDouble(lengthString);
+        double width = myApp.convertStringToDouble(widthString);
+
+        double areaInFeet = myApp.getArea(length, width, feetConversion);
+        String areaInFeetString = String.format("%.0f", areaInFeet);
+        double areaInMeters = myApp.getArea(length, width, metersConversion);
+        String areaInMetersString = String.format("%.3f", areaInMeters);
+        String message = myApp.generateMessage(lengthString, widthString, areaInFeetString, areaInMetersString);
+        System.out.println(message);
+    }
+
+    public String getLength(){
+        System.out.print("What is the length of the room in feet? ");
+        String len = input.nextLine();
+        return len;
+    }
+    public String getWidth(){
+        System.out.print("What is the width of the room in feet? ");
+        String wid = input.nextLine();
+        return wid;
+    }
+
+    public double convertStringToDouble(String word){
+        return Double.parseDouble(word);
+    }
+
+    public double getArea(double length, double width, double conversion){
+        return ((length * width) * conversion);
+    }
+
+    public String generateMessage(String length , String width, String areaFeet, String areaMeters) {
+        return "You entered dimensions of " + length + " feet by " + width + " feet.\n" +
+                "The area is\n" + areaFeet + " square feet\n" + areaMeters + " square meters";
     }
 }
--- src/test/java/oop/example/AppTest.java
+++ src/test/java/oop/example/AppTest.java
@@ -1,20 +1,33 @@
 package oop.example;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void include_correct_calculations(){
+        App myApp = new App();
+        String lengthString = "15";
+        String widthString = "20";
+        double feetConversion = 1.0;
+        double metersConversion = 0.09290304;
+
+        double length = myApp.convertStringToDouble(lengthString);
+        double width = myApp.convertStringToDouble(widthString);
+
+        double areaInFeet = myApp.getArea(length, width, feetConversion);
+        String areaInFeetString = String.format("%.0f", areaInFeet);
+        double areaInMeters = myApp.getArea(length, width, metersConversion);
+        String areaInMetersString = String.format("%.3f", areaInMeters);
+
+        String actualMessage = myApp.generateMessage(lengthString, widthString, areaInFeetString, areaInMetersString);
+        String expectedMessage = "You entered dimensions of 15 feet by 20 feet.\n" +
+                "The area is\n" +
+                "300 square feet\n" +
+                "27.871 square meters";
+
+        assertEquals(expectedMessage, actualMessage);
     }
 }
